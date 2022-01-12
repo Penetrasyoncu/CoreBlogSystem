a4bbcff611ae245efc9345d05ede1d392e0707f1
ibrahim okuyucu <ibrahimokuyucuasg@hotmail.com>
2022-01-12 14:36:12 +0300

    1436

--- CoreBlogSystem/Controllers/CommentController.cs
+++ CoreBlogSystem/Controllers/CommentController.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinnessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreBlogSystem.Controllers
 {
     public class CommentController : Controller
     {
+        CommentManager cm = new CommentManager(new EfCommentRepository());
+
         public IActionResult Index()
         {
             return View();
@@ -15,9 +19,10 @@ namespace CoreBlogSystem.Controllers
             return PartialView();
         }
 
-        public PartialViewResult CommentListByBlog()
+        public PartialViewResult CommentListByBlog(int id)
         {
-            return PartialView();
+            var values =  cm.GetList(id);
+            return PartialView(values);
         }
     }
 }
