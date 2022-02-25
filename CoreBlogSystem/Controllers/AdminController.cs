4249f5ae945432b60ba9c903908e84e3f0294867
ibrahim okuyucu <ibrahimokuyucuasg@hotmail.com>
2022-02-26 00:45:41 +0300

    AdminTemplate

---
+++ CoreBlogSystem/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using BusinnessLayer.Concrete;
+using CoreBlogSystem.Models;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace CoreBlogSystem.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Burası Admin Panel'in Navbar'ı yani sol kısmı
+        public PartialViewResult AdminNavbar()
+        {
+            return PartialView();
+        }
+    }
+}
