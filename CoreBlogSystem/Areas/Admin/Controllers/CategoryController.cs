3399ce4627f02fe8592f85518ab0fdd2fb99c3ef
ibrahim okuyucu <ibrahimokuyucuasg@hotmail.com>
2022-02-27 23:03:47 +0300

    2303

---
+++ CoreBlogSystem/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using BusinnessLayer.Concrete;
+using CoreBlogSystem.Models;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace CoreBlogSystem.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
