8c749a3fba7e692560f79a33756febc1e632241b
ibrahim okuyucu <ibrahimokuyucuasg@hotmail.com>
2022-03-08 23:59:34 +0300

    AdminİstatistikKısmı

---
+++ CoreBlogSystem/Areas/Admin/ViewComponents/Statistic/AdminStatistics.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinnessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreBlogSystem.Areas.Admin.ViewComponents.Statistic
+{
+    public class AdminStatistics : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ CoreBlogSystem/Areas/Admin/ViewComponents/Statistic/DetailsStatistics.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinnessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreBlogSystem.Areas.Admin.ViewComponents.Statistic
+{
+    public class DetailsStatistics : ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+        public IViewComponentResult Invoke()
+        {
+            Context c = new Context();
+            ViewBag.LastOneBlogName = c.Blogs.OrderByDescending(x => x.BlogID).Select(x => x.BlogTitle).Take(1).FirstOrDefault();
+            return View();
+        }
+    }
+}
