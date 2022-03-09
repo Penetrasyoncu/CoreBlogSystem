1cf86ea4aa983de11a7e7321efb4dfcb730090d2
ibrahim okuyucu <ibrahimokuyucuasg@hotmail.com>
2022-03-09 21:48:27 +0300

    Revert "AdminİstatistikKısmı"
    This reverts commit 8c749a3fba7e692560f79a33756febc1e632241b.

--- CoreBlogSystem/Areas/Admin/ViewComponents/Statistic/GeneralStatistics.cs
+++ CoreBlogSystem/Areas/Admin/ViewComponents/Statistic/GeneralStatistics.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BusinnessLayer.Concrete;
-using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,19 +12,9 @@ namespace CoreBlogSystem.Areas.Admin.ViewComponents.Statistic
     public class GeneralStatistics : ViewComponent
     {
         public IViewComponentResult Invoke()
-        {            
+        {
             BlogManager bm = new BlogManager(new EfBlogRepository());
-            Context c = new Context();
-
-            //Toplam Blog Sayısını Çektiğimiz Alan ---- Widgets
             ViewBag.AllBlogs = bm.GetList().Count();
-
-            //Toplam Mesaj Sayısını Aldığımız Kısım
-            ViewBag.MessageCount = c.Contacts.Count();
-
-            //Toplam Yorum Sayısı
-            ViewBag.CommentCount = c.Comments.Count();
-
             return View();
         }
     }
