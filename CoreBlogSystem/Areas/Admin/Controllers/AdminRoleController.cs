74980dbc949d6e9bdc2abb294618040f39730e27
ibrahim okuyucu <ibrahimokuyucuasg@hotmail.com>
2022-03-28 14:13:59 +0300

    Role Add

---
+++ CoreBlogSystem/Areas/Admin/Controllers/AdminRoleController.cs
@@ -0,0 +1,59 @@
+﻿using CoreBlogSystem.Models;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBlogSystem.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminRoleController : Controller
+    {
+
+        private readonly RoleManager<AppRole> _roleManager;
+
+        public AdminRoleController(RoleManager<AppRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            var roles = _roleManager.Roles.ToList();
+            return View(roles);
+        }
+
+        [HttpGet]
+        public IActionResult AddRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddRole(RoleViewModel roleViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                AppRole role = new AppRole
+                {
+                    Name = roleViewModel.Name
+                };
+
+                var result = await _roleManager.CreateAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+            return View(roleViewModel);
+        }
+    }
+}
